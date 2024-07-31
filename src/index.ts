18547dbbd7a3a1810a35e86b9ff515f807c51d59
AplinXy9plin <aplinxy9plin@gmail.com>
2024-07-31 14:46:27 +0700

    feat: 🎸 webapp on start

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Telegraf } from 'telegraf';
+import { Markup, Telegraf } from 'telegraf';
 
 import { about } from './commands';
 import { greeting } from './text';
@@ -7,10 +7,17 @@ import { development, production } from './core';
 
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
+const WEBAPP_LINK = process.env.WEBAPP_LINK || '';
 
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.command('about', about());
+bot.command('start', async (ctx) => {
+  ctx.reply(
+    'Launch miniapp ;)',
+    Markup.inlineKeyboard([Markup.button.webApp('Launch', WEBAPP_LINK)]),
+  );
+});
 bot.on('message', greeting());
 
 //prod mode (Vercel)
